cefae90a1d7e96cd665ec786e9c93606382e3e59
komanda.phanzu <komanda.phanzu@gmail.com>
2012-09-22 15:23:50 +0200

    before to switch in code to data way

---
+++ Nespe/Context/NespeDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data.Common;
+using Nespe.Models;
+
+namespace Nespe.Context
+{
+    public class NespeDbContext : DbContext
+    {
+        public NespeDbContext()
+            : base()
+        {
+
+        }
+        public NespeDbContext(DbConnection existingConnection, bool contextOwnsConnection)
+            :base(existingConnection, contextOwnsConnection)
+        {
+
+        }
+        public DbSet<Request> RequestSet { get; set; }
+        public DbSet<RequestTypeInfo> RequestInfoSet { get; set; }
+        public DbSet<Person> PersonSet { get; set; }
+        public DbSet<Department> DepartmentSet { get; set; }
+        public DbSet<PersonDepartment> PersonDepartmentSet { get; set; }
+    }
+}
