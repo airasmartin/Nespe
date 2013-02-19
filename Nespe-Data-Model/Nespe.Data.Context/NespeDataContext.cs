e77bca5c62c1943a63bcf0a3e347bf6f2764871d
komanda.phanzu <komanda.phanzu@gmail.com>
2013-02-19 22:29:38 +0100

    Just before some refactoring

---
+++ Nespe-Data-Model/Nespe.Data.Context/NespeDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.OleDb;
+//using System.ComponentModel.DataAnnotations.Schema;
+//using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Nespe.Data.Context
+{
+    [DbModelBuilderVersion(DbModelBuilderVersion.V5_0)]
+    public class NespeDataContext : DbContext
+    {
+    }
+}
---
+++ Nespe-Data-Model/Nespe.Data.Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nespe.Data.Entities
+{
+    public class Department
+    {
+        public virtual long Id { get; set; }
+        public virtual string SID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Entity { get; set; }
+        public virtual string EMail { get; set; }
+        public virtual string Phone { get; set; }
+
+    }
+}
