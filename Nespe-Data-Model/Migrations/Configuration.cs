d3bf0ae5cd7a703e0af8286a0c48176ff2f1a439
komanda.phanzu <komanda.phanzu@gmail.com>
2013-02-21 20:19:30 +0100

    Just some new features

---
+++ Nespe-Data-Model/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Nespe.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Nespe.Data.Context.NespeDataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Nespe.Data.Context.NespeDataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
